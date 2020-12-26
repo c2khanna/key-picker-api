096a85031e3eb6e2dfc1620e34c00996933b3cd9
c2khanna <chaitanyakhanna13@gmail.com>
2020-12-26 03:32:56 -0500

    implemented cors

--- server/index.js
+++ server/index.js
@@ -10,19 +10,23 @@ const port = 3000
 
 app.use(bodyParser.json());
 
-// var whitelist = ['http://localhost:4200']
-
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-
-app.use(cors());
+if(process.env.NODE_ENV === 'development') {
+  app.use(cors());
+} else {
+  var whitelist = ['http://keypicker.s3-website.us-east-2.amazonaws.com'];
+  
+  var corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+  
+  app.use(cors(corsOptions));
+}
 
 app.use(router);
 
