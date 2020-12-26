4a1ca22ce85694414d58f23a23927f69c9efde90
c2khanna <chaitanyakhanna13@gmail.com>
2020-12-26 01:21:24 -0500

    fixed data mapping for filtered keyboards

--- server/services/keyboardService.js
+++ server/services/keyboardService.js
@@ -79,7 +79,20 @@ const filterKeyboardService = (req, res) => {
       })
     }
   }).then(data => {
-    res.status(200).send(data);
+    let keyboards = data.map((k) => {
+      return {
+        brand: k.brand,
+        model: k.model,
+        price: k.price,
+        link: k.url,
+        imgLink: k.img_url,
+        switchType: k.switch_type,
+        size: k.size,
+        isWireless: k.is_wireless,
+        light: k.light
+      }
+    });
+    res.status(200).send(keyboards);
   })
 }
 
